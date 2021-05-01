39d4ce676402089dcecd0b85591856dacd9e0747
koonn <konnie211@gmail.com>
2021-05-01 11:03:29 +0900

    :tada: Add package

---
+++ src/functions.py
@@ -0,0 +1,40 @@
+"""活性化関数
+"""
+
+def step_function(x1, x2):
+    """ステップ関数
+
+    Args:
+        x1: 変数1の入力値
+        x2: 変数2の入力値
+
+    Returns: 活性化関数によって変換した出力値shuturyokuatai
+
+    """
+    pass
+
+
+def sigmoid_function(x1, x2):
+    """シグモイド関数
+
+    Args:
+        x1: 変数1の入力値
+        x2: 変数2の入力値
+
+    Returns: 活性化関数によって変換した出力値shuturyokuatai
+
+    """
+    pass
+
+
+def relu_function(x1, x2):
+    """ReLU
+
+    Args:
+        x1: 変数1の入力値
+        x2: 変数2の入力値
+
+    Returns: 活性化関数によって変換した出力値shuturyokuatai
+
+    """
+    pass
