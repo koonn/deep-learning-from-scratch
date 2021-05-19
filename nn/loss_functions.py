fd97696183a9a030f72156bbc7130bfb9fe5df83
koonn <konnie211@gmail.com>
2021-05-20 00:32:31 +0900

    :+1: 交差エントロピー誤差を追加

--- nn/loss_functions.py
+++ nn/loss_functions.py
@@ -15,3 +15,18 @@ def mean_squared_error(y, t):
         float: 最小二乗誤差の値
     """
     return 0.5 * np.sum((y - t)**2)
+
+
+def cross_entropy_error(y, t):
+    """交差エントロピー誤差を計算する関数
+
+    Args:
+        y (np.array): 予測値の配列
+        t (np.array): 実データ値の配列
+
+    Returns:
+        float: 交差エントロピー誤差の値
+    """
+    delta = 1e-7
+
+    return -np.sum(t * np.log(y + delta))
