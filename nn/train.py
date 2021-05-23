bcbefa71e90750bceb5b2d9be4e28c9a720522b5
koonn <konnie211@gmail.com>
2021-05-24 08:19:59 +0900

    :sparkles: 複数変数の偏微分のための関数を作成

--- nn/train.py
+++ nn/train.py
@@ -1,10 +1,55 @@
 """NNの学習用の関数群
 """
+import numpy as np
 
 
 def numerical_diff(f, x):
     """数値微分のための関数
+
+    Args:
+        f (function): 微分を求めたい関数(1変数)
+        x (numeric): 微分を求めたい点(x座標)
+
+    Returns:
+
     """
     h = 1e-4
 
     return (f(x+h) - f(x-h)) / (2*h)
+
+
+def numerical_gradient(f, x):
+    """
+
+    Args:
+        f (function): 偏微分を求めたい関数
+        x (numeric): 偏微分を求めたい点(x座標)
+
+    Returns:
+
+    Examples:
+        functionの形式は以下のように、numpy配列を引数にとるものを想定
+
+        def function_2(x):
+            return x[0]**2 + x[1]**2
+    """
+    h = 1e-4
+
+    grad = np.zeros_like(x)  # xと同じ形状の配列を作成
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+
+        # f(x+h)の計算
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h)の計算
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        # 勾配の計算
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val #値を元に戻す
+
+    return grad
